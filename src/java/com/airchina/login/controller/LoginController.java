d934ba06e7b128a49e0ac9ee508c8979a0e8bdae
Yanbing Tan <tanyanbing@airchina.com>
2015-12-31 16:31:11 +0800

    login
    添加登录页面

---
+++ src/java/com/airchina/login/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.airchina.login.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fes.test.model.Test;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView system(HttpServletRequest request, HttpSession session, Test tt) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		return mav;
+	}
+}
